af1204e72318533c38ee587fe7b6ed821f98a003
Dsdevv001mf <cordobel@live.com>
2021-04-08 15:42:14 -0300

    Errando de propósito pra ver o que acontece no azure pipeline

--- Tailspin.SpaceGame.Web/Models/ErrorViewModel.cs
+++ Tailspin.SpaceGame.Web/Models/ErrorViewModel.cs
@@ -2,7 +2,7 @@ namespace TailSpin.SpaceGame.Web.Models
 {
     public class ErrorViewModel
     {
-        public string RequestId { get; set; }
+        public string RequestId { get; fasdfas; }
 
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
     }
